26e2a995a4e33d68f2478c69ee9162ed8b463b7c
Kyle <Kyle@10.21.16.172>
2015-09-16 11:29:54 -0600

    Working piece. added ranadom number to end of password

--- Exercise_2/Exercise_2/src/Exercise_2/Exercise_2.java
+++ Exercise_2/Exercise_2/src/Exercise_2/Exercise_2.java
@@ -12,6 +12,7 @@ public class Exercise_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String password;
 		int x; // declare x as an integer
 		float f; // declare variable pi as a float
 		
@@ -21,7 +22,7 @@ public class Exercise_2 {
 		int randomInt = randomGenerator.nextInt(100000); // Declares a value that has 5 digits and is random.
 		int randomInt2 = randomGenerator.nextInt(10); // declares a seccond rnadom to be added to the first
 		float randF = randomGenerator.nextFloat();
-		int wh = 1; // declare wh as a check value
+		
 		
 	
 		while(true){ //run this code until the user input is correct and wh is then 1.
@@ -40,6 +41,7 @@ public class Exercise_2 {
 		System.out.println("Please enter this decimal: " + randF);
 		System.out.println(randF);
 		f = scan1.nextFloat(); // assigns pi to a flat value entered by user
+
 		if (f == randF && x == y){
 			
 			System.out.println("Thank you. Your identity has been identified.");
@@ -60,12 +62,40 @@ public class Exercise_2 {
 			System.out.println("Welcome. Please follow the instructions on the next screen.");
 			try{Thread.sleep(2000);}
 			catch(Exception e){};
-
-			//jPane();
-		
+			//JOptionPane.showMessageDialog(null,  "HEllo");
+			
+				
+			//JOptionPane.showInputDialog("what is your name?");
+			int randomPass = randomGenerator.nextInt(10000);
+			//String strPass = String.valueOf(randomPass);
+			
+			JOptionPane.showMessageDialog(null, "You will now create a unique password for your account.");
+				
+			try{Thread.sleep(2000);}
+			catch(Exception e){};
+				
+			String ans1 = JOptionPane.showInputDialog(null, "What is your last name?");
+				
+			try{Thread.sleep(1500);}
+			catch(Exception e){};
+				
+			String ans2 = JOptionPane.showInputDialog(null, "Please enter the name off your hometown.");
+				
+			try{Thread.sleep(2000);}
+			catch(Exception e){};
+				
+			JOptionPane.showMessageDialog(null, "Thank you, I will now create your unqiue password. PLease wait...");
+				
+			try{Thread.sleep(2000);}
+			catch(Exception e){};
+			password = ans1.substring(0, 3) + ans2.substring(5) + randomPass;
+			//String password = (ans1.substring(0, 2)) + (ans2.substring(3, ans2.charAt(ans2.length()-1)) + (strPass)); // last character codde found at http://www.coderanch.com/t/395725/java/java/substring-character
+			
+			JOptionPane.showMessageDialog(null, "Your unqiue password is: " + password);
 		
 		
 		scan1.close();
 	}
-	
-}
+	}
+			
+
