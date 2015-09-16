02d3328defea4bd80282bc55b035547140318383
Kyle <Kyle@10.21.16.172>
2015-09-16 08:17:26 -0600

    began adding the second class with the function that creates a unique password

--- Exercise_2/Exercise_2/src/Exercise_2/Exercise_2.java
+++ Exercise_2/Exercise_2/src/Exercise_2/Exercise_2.java
@@ -13,32 +13,34 @@ public class Exercise_2 {
 		// TODO Auto-generated method stub
 		
 		int x; // declare x as an integer
-		double pi; // declare variable pi as a float
+		float f; // declare variable pi as a float
 		
 		// declare a new scanner to scan the input.
 		Scanner scan1 = new Scanner(System.in);
 		Random randomGenerator = new Random(); // start a new random number generator
 		int randomInt = randomGenerator.nextInt(100000); // Declares a value that has 5 digits and is random.
 		int randomInt2 = randomGenerator.nextInt(10); // declares a seccond rnadom to be added to the first
+		float randF = randomGenerator.nextFloat();
 		int wh = 1; // declare wh as a check value
 		
 	
 		while(true){ //run this code until the user input is correct and wh is then 1.
-		int count = 6 - wh;
-		System.out.println("Please verify identity: Tries remaining: " + count); // prints a line
+		System.out.println("Please verify identity"); // prints a line
 		System.out.println("Please enter the sum of the numbers: " + randomInt + " + " + randomInt2 ); // prints a line
 		
 		//System.out.println(randomInt);
 		//System.out.println(randomInt2);
 		//System.out.println(randomInt + randomInt2);
+		
 		int y = randomInt + randomInt2;
-		wh = wh++;
+		
 		
 		
 		x = scan1.nextInt(); // assign latest input to x
-		System.out.println("Please enter that value of Pi to 4 decimal places (3.1415)");
-		pi = scan1.nextFloat(); // assigns pi to a flat value entered by user
-		if (pi == 3.1415 && x == y){
+		System.out.println("Please enter this decimal: " + randF);
+		System.out.println(randF);
+		f = scan1.nextFloat(); // assigns pi to a flat value entered by user
+		if (f == randF && x == y){
 			
 			System.out.println("Thank you. Your identity has been identified.");
 			try{Thread.sleep(2000);}
@@ -51,16 +53,15 @@ public class Exercise_2 {
 			try{Thread.sleep(1200);}
 			catch(Exception e){};
 		}
-		if(wh == 6){
-			System.exit(1); //Found online at stackoverflow.com
-		}
+		
+
 		}
 		
 			System.out.println("Welcome. Please follow the instructions on the next screen.");
 			try{Thread.sleep(2000);}
 			catch(Exception e){};
 
-			//public jPane();
+			//jPane();
 		
 		
 		
